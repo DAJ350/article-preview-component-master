7a4fdec9ff5c5569df7d5084ebcc88e2a1cd3a91
DAJ350 <daniellajorin@gmail.com>
2024-10-22 18:24:36 +0100

    javscript styling update

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const shareButton = document.getElementById("share-btn");
+const shareButtonCircle = document.getElementById("share-btn-circle");
+const shareButtonArrow = document.getElementById("share-btn-arrow");
 const popup = document.getElementById("popup");
 
 function togglePopup() {
-  if (popup.style.display === "none") {
+  if (popup.style.display == "none") {
+    shareButtonCircle.style.fill = "var(--alternate-color)";
+    shareButtonArrow.style.fill = "var(--card-bg)"
     popup.style.display = "flex";
   } else {
+    shareButton.style.fill = "none";
+    shareButtonCircle.style.fill = "var(--main-bg)";
+    shareButtonArrow.style.fill = "var(--alternate-color)";
     popup.style.display = "none";
   }
 }
