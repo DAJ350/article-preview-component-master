52b57dec928dba14207bf5b9480c402d245a9a84
DAJ350 <daniellajorin@gmail.com>
2024-10-24 19:57:02 +0100

    project first attempt completion

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ const shareButton = document.getElementById("share-btn");
 const shareButtonCircle = document.getElementById("share-btn-circle");
 const shareButtonArrow = document.getElementById("share-btn-arrow");
 const popup = document.getElementById("popup");
+const shareButtonContainer = document.getElementById("share-btn-container");
 
 function togglePopup() {
-  if (popup.style.display == "none") {
+  if (popup.style.display == "none" || popup.style.display == "") {
     shareButtonCircle.style.fill = "var(--alternate-color)";
-    shareButtonArrow.style.fill = "var(--card-bg)"
+    shareButtonArrow.style.fill = "var(--card-bg)";
     popup.style.display = "flex";
   } else {
     shareButton.style.fill = "none";
     shareButtonCircle.style.fill = "var(--main-bg)";
     shareButtonArrow.style.fill = "var(--alternate-color)";
     popup.style.display = "none";
+  }
+
+  if (document.body.clientWidth >= 375 && document.body.clientWidth < 425) {
+    shareButtonContainer.classList.toggle(
+      "preview-card__share-button-container--active-mobile"
+    );
   }
 }
 
